c537b2263e251b5142e5efe9da09ebfd22ee00b9
Peter Loes <loes@orn.mpg.de>
2021-07-05 13:36:53 +0200

    Add files via upload

--- Software/drivers/SensorMon.h
+++ Software/drivers/SensorMon.h
@@ -79,7 +79,7 @@ typedef enum
     SBS_REPEATABILITY_CLOCK_MPS_10_MEDIUM   = 0x3022721,  //!< 0X2721 10 mps
     SBS_REPEATABILITY_CLOCK_MPS_10_LOW      = 0x302272A,  //!< 0X272A 10 mps
     SBS_READ_SERIAL_NUMBER    = 0x3023780,  //!< 0x3780 Serial Number
-    SBS_FETCH_DATA            = 0x302E000,  //!< 0xE000 Fetch Data
+    SBS_FETCH_DATA            = 0x306E000,  //!< 0xE000 Fetch Data
     SBS_ART_MEASUREMENT       = 0x3022B32,  //!< 0x2B32 ART Periodic Measurement
     SBS_BREAK                 = 0x3023093,  //!< 0x3093 Break
     SBS_SOFT_RESET            = 0x30230A2,  //!< 0x30A2 Soft Reset 
